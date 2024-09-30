5f9174e35f9a41acc635aef371997a66fc232877
Benn <bennkiche7@gmail.com>
2024-10-01 00:59:19 +0300

    submittask

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+const batteryBatches = [4, 5, 3, 4, 4, 6, 5]; 
+
+// Using reduce to sum up the battery amounts
+const totalBatteries = batteryBatches.reduce((accumulator, current) => {
+  return accumulator + current;
+}, 0); // Starting with an initial value of 0
+
+console.log(totalBatteries); // This will log the total number of batteries
